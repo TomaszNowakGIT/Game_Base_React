5337b13b778930ad557d2459e4485bee6185ecd4
Tomasz Nowak <tn@mpro.pl>
2019-09-16 20:24:44 +0200

    Updates

---
+++ precache-manifest.cb96309d3a83ceca24d90ea70534ed5e.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ee3c478d5066097a665774b76da43346",
+    "url": "/GameBase/index.html"
+  },
+  {
+    "revision": "8cef86ac24a6dd75a1fb",
+    "url": "/GameBase/static/css/main.c3de58f1.chunk.css"
+  },
+  {
+    "revision": "88d26bfb130600ca339e",
+    "url": "/GameBase/static/js/2.be97a4d3.chunk.js"
+  },
+  {
+    "revision": "8cef86ac24a6dd75a1fb",
+    "url": "/GameBase/static/js/main.b580a7f8.chunk.js"
+  },
+  {
+    "revision": "cc159f24bbaf3696526a",
+    "url": "/GameBase/static/js/runtime~main.aeee98e8.js"
+  },
+  {
+    "revision": "53e8bb1f29b0c0f0c2822c6720f2853a",
+    "url": "/GameBase/static/media/backbrick.53e8bb1f.jpg"
+  },
+  {
+    "revision": "276094fe313585aef1f93735d30b0b0b",
+    "url": "/GameBase/static/media/cat.276094fe.wav"
+  },
+  {
+    "revision": "df84d0e7c378fc804ee71ab344da073a",
+    "url": "/GameBase/static/media/crt.df84d0e7.png"
+  },
+  {
+    "revision": "44db2cdb81f9ff35faa2a1b0f713effa",
+    "url": "/GameBase/static/media/failure.44db2cdb.wav"
+  },
+  {
+    "revision": "c264143a257e5ed905207e1283802d8f",
+    "url": "/GameBase/static/media/jump.c264143a.wav"
+  },
+  {
+    "revision": "82baf3cdd9854b0aaf6fcc1509ff429b",
+    "url": "/GameBase/static/media/loop.82baf3cd.mp3"
+  }
+]);
