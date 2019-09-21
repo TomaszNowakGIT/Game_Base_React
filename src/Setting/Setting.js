6f72c4fb782fc33ae4e6fe3d48c276a089e339a8
Tomasz Nowak <tn@mpro.pl>
2019-09-21 16:02:52 +0200

    Dodanie kosza na smieci

--- src/Setting/Setting.js
+++ src/Setting/Setting.js
@@ -2,6 +2,7 @@ import React from "react";
 import "./Setting.css";
 import { EventEmitter } from "../EventEmitter";
 import lang from "../lang.json";
+import trash from "./trash.mp3";
 
 class Setting extends React.Component {
   setLanguage(lang) {
@@ -10,6 +11,25 @@ class Setting extends React.Component {
   }
   resetScore() {
     localStorage.setItem("highScore", 0);
+  }
+  trashSoundStart() {
+    let trash;
+    trash = document.getElementById("trash");
+    trash.volume = 0.1;
+    trash.play();
+    this.trashAnimationStart();
+  }
+  trashAnimationStart() {
+    let trashSmell;
+    trashSmell = document.getElementById("smells");
+    trashSmell.style.animationPlayState = "running";
+    trashSmell.style.display = "block";
+  }
+  trashAnimationStop() {
+    let trashSmell;
+    trashSmell = document.getElementById("smells");
+    //trashSmell.style.animationPlayState = "running";
+    trashSmell.style.display = "none";
   }
 
   render() {
@@ -38,6 +58,15 @@ class Setting extends React.Component {
         </div>
         <div className='plate'>
           created by <a href={"mailto:tn@mpro.pl"}>MPRO</a>
+        </div>
+        <div
+          className='trash'
+          onMouseEnter={this.trashSoundStart.bind(this)}
+          onMouseLeave={this.trashAnimationStop.bind(this)}>
+          <audio id='trash' src={trash} style={{ display: "none" }}></audio>
+          <div className='pokrywka'></div>
+          <div className='paski'></div>
+          <div id='smells' className='smells'></div>
         </div>
       </div>
     );
